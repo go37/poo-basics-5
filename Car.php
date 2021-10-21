1403069b5d561b66c64066df8c1fb055d1512eeb
Olivier Gourdonneau <olivier.gourdonneau@meteo.fr>
2021-10-21 12:06:23 +0200

    first commit for poo-basics-3

---
+++ Car.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'Vehicle.php';
+
+class Car extends Vehicle
+{
+
+    private int $energyLevel;
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->setEnergy($energy);
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+    
+}
---
+++ Vehicle.php
@@ -0,0 +1,99 @@
+<?php
+class Vehicle
+{
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+    protected string $color;
+
+    protected int $currentSpeed;
+
+    protected int $nbSeats;
+
+    protected int $nbWheels;
+
+    protected string $energy;
+
+    public function __construct(string $color, int $nbSeats)
+    {
+        $this->color = $color;
+        $this->nbSeats = $nbSeats;
+    }
+
+    public function forward(): string
+    {
+        $this->currentSpeed = 15;
+        return "Go !" . PHP_EOL;
+    }
+
+    public function brake(): string
+    {
+        $sentence = "";
+
+        while ($this->currentSpeed > 0) {
+            $this->currentSpeed--;
+            $sentence .= "Brake !!!" . PHP_EOL;
+        }
+
+        $sentence .= "I'm stopped !" . PHP_EOL;
+        return $sentence;
+    }
+
+    public function getCurrentSpeed(): int
+    {
+        return $this->currentSpeed;
+    }
+
+    public function setCurrentSpeed(int $currentSpeed): void
+    {
+        if($currentSpeed >= 0){
+            $this->currentSpeed = $currentSpeed;
+        }
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getNbSeats(): int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats(int $nbSeats): void
+    {
+        $this->nbSeats = $nbSeats;
+    }
+
+    public function getNbWheels(): int
+    {
+        return $this->nbWheels;
+    }
+
+    public function setNbWheels(int $nbWheels): void
+    {
+        $this->nbWheels = $nbWheels;
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy)
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+}
